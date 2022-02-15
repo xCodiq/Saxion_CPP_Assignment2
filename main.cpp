214a956b825c085f94a0ee2eb3b5bc1f51593ac2
xCodiq <elmarblume91@gmail.com>
2022-02-15 10:29:59 +0100

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+ * C++ Foundations - SAXION 2022
+ *
+ * Project: Assignment 2
+ * Datum: 15-02-2022
+ * Door: Elmar Blume
+ */
+
+#include <iostream>
+
+using std::cout, std::cin, std::endl, std::getline, std::ifstream, std::ofstream, std::string, std::isalpha, std::islower;
+
+int main() {
+    return 0;
+}
